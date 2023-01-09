c4b0686bc7bd0f9a8271b345cf268f1b521bd722
ofekPr <70010761+ofekPr@users.noreply.github.com>
2023-01-09 11:36:32 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -105,6 +105,18 @@ public class Main {
         return t1;
     }
 
+    public static Node<Integer> removeInBoth(Node<Integer> p1, Node<Integer> p2) {  //ex1-10
+        Node<Integer> finalP = p1;
+        while(p1 != null) {
+            int num = p1.getValue();
+            if (findNum(p2, num, false)) {
+                finalP = removeNum(finalP, num);
+            }
+            p1 = p1.getNext();
+        }
+        return finalP;
+    }
+
     public static Node<Integer> mergeList(Node<Integer> p1, Node<Integer> p2) {  //ex2-1
         Node<Integer> finalP;
         if (p2.getValue() < p1.getValue()) {
@@ -114,7 +126,7 @@ public class Main {
             finalP = new Node<Integer>(p1.getValue());
             p1 = p1.getNext();
         }
-        Node<Integer> retrurnP = finalP;
+        Node<Integer> returnP = finalP;
         while(p1 != null || p2 != null) {
             if (p1 == null || p2.getValue() < p1.getValue()) {
                 finalP.setNext(new Node<Integer>(p2.getValue()));
@@ -126,24 +138,94 @@ public class Main {
                 p1 = p1.getNext();
             }
         }
-        return retrurnP;
+        return returnP;
+    }
+
+    public static Node<Integer> selectionSort(Node<Integer> p1) {  // ex2-2
+        Node<Integer> finalP = p1;
+        while(p1 != null) {
+            Node<Integer> p2 = p1;
+            int min = Integer.MAX_VALUE;
+            Node<Integer> minLoc = p2;
+            while(p2 != null) {
+                if (p2.getValue() < min) {
+                    min = p2.getValue();
+                    minLoc = p2;
+                }
+                p2 = p2.getNext();
+            }
+            minLoc.setValue(p1.getValue());
+            p1.setValue(min);
+            p1 = p1.getNext();
+        }
+        return finalP;
+    }
+
+    public static int sumDistance(Node<Integer> p1, int value) {  // ex2-3
+        if (!findNum(p1, value, 8)) {
+            return -1;
+        }
+        int length = p1.len();
+        int sum = 0, index = 0;
+        while(p1 != null) {
+            if (p1.getValue() == value) {
+                sum += Math.min(Math.abs(index), Math.abs(length - index - 1));
+            }
+            index++;
+            p1 = p1.getNext();
+        }
+        return sum;
+    }
+
+    public static void printNewExe(String str){
+        System.out.println("\n**********************************************\n" + str);
     }
 
     public static void main(String[] args) {
+        printNewExe("Initializing lists:");
         int[] arr =  {1, 2, 3, 4, 5, 6, 7};
-        int[] arr2 =  {0, 8, 9};
+        int[] arr2 =  {0, 1, 2, 8, 9};
         Node<Integer> p1 = arrToList(arr);
         System.out.println(p1);
         Node<Integer>  p2 = arrToList(arr2);
         System.out.println(p2);
-//        printEven(p2);
-//        System.out.println(findNum(p2, 2, false));
-//        System.out.println(findNum(p2, 5, false));
-//        System.out.println(findNum(p2, 1000, false));
-//        System.out.println(removeNum(p2, 7));
-//        System.out.println(isAllInList(p1, p2));
-//        printInBoth(p1, p2);
-//        System.out.println(listWithBoth(p1, p2));
+
+        printNewExe("Printing even numbers only in p2:");
+        printEven(p2);
+
+        printNewExe("Finding nums in p2:");
+        System.out.println("2: " + findNum(p2, 2, false));
+        System.out.println("5: " + findNum(p2, 5, false));
+        System.out.println("100: " + findNum(p2, 1000, false));
+
+        printNewExe("Removing 8 from p2:");
+        System.out.println(removeNum(p2, 8));
+
+        printNewExe("Is p1 in p2");
+        System.out.println(isAllInList(p1, p2));
+
+        printNewExe("Printing values in both lists:");
+        printInBoth(p1, p2);
+
+        printNewExe("list with values only in both lists:");
+        System.out.println(listWithBoth(p1, p2));
+
+        printNewExe("remove from p1 values in both lists:");
+        System.out.println(p1);
+        System.out.println(removeInBoth(p1, p2));
+
+        printNewExe("Merged list:");
         System.out.println(mergeList(p1, p2));
+        int[] arr3 = {9, 2, 8, 3, 8, 5};
+        Node<Integer> p3 = arrToList(arr3);
+
+        printNewExe("New p3:");
+        System.out.println(p3);
+
+        printNewExe("Sum distances on p3 from 8:");
+        System.out.println(sumDistance(p3, 8));
+
+        printNewExe("Sort p3:");
+        System.out.println(selectionSort(p3));
     }
 }
